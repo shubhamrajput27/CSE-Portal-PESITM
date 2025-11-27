be359afc82f958762b1037ee4f47ac78d244e9c2
Shubham Singh <shubhamrajput2702@gmail.com>
2025-11-27 12:11:56 +0530

    Add: Request logging middleware to server

--- server/server.js
+++ server/server.js
@@ -31,6 +31,12 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Request logging middleware
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} - ${req.method} ${req.path}`)
+  next()
+})
+
 // Database connections
 const connectDatabases = async () => {
   // PostgreSQL connection test
